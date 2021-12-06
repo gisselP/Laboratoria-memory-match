05ad136d2bc99b83eedaffd3ae16bd2fdc7d57f1
MaffPerez <plzamfr@gmail.com>
2021-12-06 12:52:30 -0500

    Change the image of the data

--- src/data/pokemon/pokemon.js
+++ src/data/pokemon/pokemon.js
@@ -1,14 +1,14 @@
 export default {
     name: 'Pokemon',
     items: [
-      { id: 'bulbasaur', image: 'https://www.serebii.net/pokemongo/pokemon/001.png', bgColor: '#339933' },
-      { id: 'ivysaur', image: 'https://www.serebii.net/pokemongo/pokemon/002.png', bgColor: '#339933' },
-      { id: 'venusaur', image: 'https://www.serebii.net/pokemongo/pokemon/003.png', bgColor: '#339933' },
-      { id: 'charmander', image: 'https://www.serebii.net/pokemongo/pokemon/004.png', bgColor: '#F05032' },
-      { id: 'charmeleon', image: 'https://www.serebii.net/pokemongo/pokemon/005.png', bgColor: '#F05032' },
-      { id: 'charizard', image: 'https://www.serebii.net/pokemongo/pokemon/006.png', bgColor: '#F05032' },
-      { id: 'squirtle', image: 'https://www.serebii.net/pokemongo/pokemon/007.png', bgColor: '#1572B6' },
-      { id: 'wartortle', image: 'https://www.serebii.net/pokemongo/pokemon/008.png', bgColor: '#1572B6' },
-      { id: 'blastoise', image: 'https://www.serebii.net/pokemongo/pokemon/009.png', bgColor: '#1572B6' },
+      { id: 'bulbasaur', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png', bgColor: '#339933' },
+      { id: 'ivysaur', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png', bgColor: '#339933' },
+      { id: 'venusaur', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png', bgColor: '#339933' },
+      { id: 'charmander', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png', bgColor: '#F05032' },
+      { id: 'charmeleon', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png', bgColor: '#F05032' },
+      { id: 'charizard', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png', bgColor: '#F05032' },
+      { id: 'squirtle', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png', bgColor: '#1572B6' },
+      { id: 'wartortle', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png', bgColor: '#1572B6' },
+      { id: 'blastoise', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png', bgColor: '#1572B6' },
     ],
   };
